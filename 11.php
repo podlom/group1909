9a7583be935afea16cf4082792b6227ec0cbafe9
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-10-11 14:00:19 +0300

    Update 11.php
    Лучше так, хотя ваш вариант тоже рабочий и ошибок в нем нет

--- 11.php
+++ 11.php
@@ -1,6 +1,17 @@
 <?php
+
 $day = 7;
-switch($day){
-    case ($day >=1 && $day <= 5): echo 'Это рабочий день'; break;
-    case ($day >=6 && $day <= 7): echo 'Это выходной день'; break;
+
+switch ($day) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+        echo 'Это рабочий день'; 
+    break;
+    case '6':
+    case '7':
+        echo 'Это выходной день';
+    break;
 }
